6e7053557dd72f34fb2cab4682b62ce65823683c
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2020-06-15 21:46:37 +0530

    release api fixed

--- example/android/app/src/main/java/io/flutter/plugins/shareexample/MainActivity.java
+++ example/android/app/src/main/java/io/flutter/plugins/shareexample/MainActivity.java
@@ -6,15 +6,16 @@ package io.flutter.plugins.shareexample;
 
 import android.os.Bundle;
 
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugins.share.FlutterShareReceiverActivity;
-
+import io.flutter.embedding.android.FlutterActivity;
 public class MainActivity extends FlutterShareReceiverActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		GeneratedPluginRegistrant.registerWith(this);
+		GeneratedPluginRegistrant.registerWith((PluginRegistry) this);
 
 	}
 }
